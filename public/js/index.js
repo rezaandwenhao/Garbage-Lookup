368c444b2c86d61400c32a47f77b9e36273a39a7
Wenhao <owengwh123@gmail.com>
2019-01-18 02:45:13 -0500

    the star can be clicked to green now

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
-let button = document.getElementById('button');
+let searchButton = document.getElementById('searchButton');
 
-button.addEventListener('click', () => {
+searchButton.addEventListener('click', () => {
     relatedObjects = new Array();   // clear the previous search
 
     if (document.getElementById('textField').value.trim() == "") {
@@ -9,10 +9,32 @@ button.addEventListener('click', () => {
     searchObject(document.getElementById('textField').value);
     console.log(relatedObjects);    // debug
     document.getElementById('resultList').innerHTML = insertResults();
+    addEventListeners();
 });
 
 var allObjects = new Array();
 var relatedObjects = new Array();
+var clickMap = new Map();
+var currentTitle = "";
+
+function createMap() {
+    for (i = 0; i < allObjects.length; i++) {
+        clickMap.set(allObjects[i].title, "buttonGrey");
+    }
+    console.log(clickMap);
+}
+
+function addEventListeners() {
+    for (i = 0; i < relatedObjects.length; i++) {
+            document.getElementById(relatedObjects[i].title)
+            .addEventListener('click', (e) => {
+                clickMap.set(e.target.id, "buttonGreen");
+                e.target.outerHTML = 
+                "<span class=\"glyphicon glyphicon-star " + clickMap.get(e.target.id) 
+                + " id=\""+ e.target.id +"\"" + " aria-hidden=\"true\"></span>";
+            });
+    }
+}
     
 function loadObjects() {
     $.getJSON('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000', function (data) {
@@ -22,12 +44,12 @@ function loadObjects() {
     })
     .done(function() {
         console.log( "JSON loaded!" );  // debug
+        createMap();
     });
 
 }
 
 function searchObject(text) {
-    var i;
     for (i = 0; i < allObjects.length; i++) {
         if (allObjects[i].title.includes(text)) {
             relatedObjects.push(allObjects[i]);
@@ -40,7 +62,12 @@ function searchObject(text) {
 function insertResults() {
     var res = "";
     for(n=0;n<relatedObjects.length;n++){
-        res = res + "<div class=\"column1\">" + relatedObjects[n].title + "</div>";
+        res = res + "<div class=\"column1\">" 
+        + "<button type=\"button\" class=\"button\">"
+        + "<span class=\"glyphicon glyphicon-star " + clickMap.get(relatedObjects[n].title) 
+        + "\"" + " id=\""+ relatedObjects[n].title +"\"" + " aria-hidden=\"true\"></span>"
+        + "</button>"
+        + relatedObjects[n].title + "</div>";
         res = res + "<div class=\"column2\">" + restoreText(relatedObjects[n].body) + "</div>";
     }
     return res;
